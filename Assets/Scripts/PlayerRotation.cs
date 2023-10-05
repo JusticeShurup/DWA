9ba2d02954b8c1a02ed9c6b33e28ce18e46dd023
Grishin Michael <69842449+JusticeShurup@users.noreply.github.com>
2023-10-06 01:01:29 +0500

    Rotation script bug fixes

--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -8,20 +8,33 @@ public class PlayerRotation : MonoBehaviour
 {
     [SerializeField] private Transform _character;
 
-    private float prevMouseCoord = 0;
     public float _rotationSpeed = 1;
+
+    private float _prevMouseCoord = 0;
+    private bool _isTouchStarted = false;
 
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton((int)MouseButton.Left))
+        if (Input.GetMouseButton((int)MouseButton.Right))
         {           
-            _character.Rotate(0, (-Input.mousePosition.x + prevMouseCoord) * _rotationSpeed, 0);
-            prevMouseCoord = Input.mousePosition.x;
+            if (!_isTouchStarted)
+            {
+                _prevMouseCoord = Input.mousePosition.x; // When click the button for the first time, the difference (-Input.mousePosition.x + _prevMouseCoord) should be zero
+                _isTouchStarted = true;
+            }
+
+            _character.Rotate(0, (-Input.mousePosition.x + _prevMouseCoord) * _rotationSpeed, 0);
+            _prevMouseCoord = Input.mousePosition.x;
+        }
+        else
+        {
+            _isTouchStarted = false;
         }
     }
 }
